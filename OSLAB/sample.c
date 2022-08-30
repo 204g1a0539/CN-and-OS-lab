eaa376375b5f590b20e990b438bd64fb2ed1fb93
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-08-30 11:41:43 +0530

    K.Hemalatha Created sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+  */
