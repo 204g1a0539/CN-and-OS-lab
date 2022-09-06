fa130ebe8b193314c8b2b7c8619dddfc8a0ac5f2
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-09-06 12:16:07 +0530

    Create RR.c

---
+++ OSLAB/RR.c
@@ -0,0 +1,7 @@
+
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: CPU Scheduling Using RR Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
