6820dd65b3f29da68fce2a9e5bcb9f1b6fa4fe2f
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-09-06 12:13:15 +0530

    Update SJP.c

--- OSLAB/SJP.C
+++ OSLAB/SJP.C
@@ -1,6 +1,6 @@
 /* Name Of the Candidate: K.Hemalatha
    Roll No: 204G1A0539
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Title of the Experiment: CPU Scheduling Using SJP Algorithm
    Date of Creation: 06-09-2022
    Date of Execution: 06-09-2022
 */
