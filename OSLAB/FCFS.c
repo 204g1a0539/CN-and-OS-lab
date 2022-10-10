1d48bdfcf21e495e5678d5e02da5cd28aef088c5
204G1A0539 <204G1A0539@srit.ac.in>
2022-10-10 15:32:38 +0530

    First list of Errors During Compilation.

--- OSLAB/FCFS.c
+++ OSLAB/FCFS.c
@@ -5,8 +5,8 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-#include<stdlib.h>
-main()
+
+int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
@@ -33,5 +33,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+return 0;
 }
