f8344f91f22d51be3657ff3a711e8862683e5f7d
204G1A0539 <204G1A0539@srit.ac.in>
2022-10-11 12:25:21 +0530

    updated

--- OSLAB/SJP.C
+++ OSLAB/SJP.C
@@ -42,5 +42,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-   return0;
+   return 0;
 }
