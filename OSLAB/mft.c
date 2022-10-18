fafbc46dce2b8789266482df10e2b90684574c3d
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-10-18 12:53:46 +0530

    Create mft.c

---
+++ OSLAB/mft.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 18-10-2022
+   Date of Execution: 18-10-2022
+*/
