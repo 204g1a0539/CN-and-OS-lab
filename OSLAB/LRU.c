62ca80d7817c56738d308d20ee817fce628178bc
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-11-01 12:33:21 +0530

    Create LRU.c

---
+++ OSLAB/LRU.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: CPU Scheduling Using fifo Algorithm
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/
