b676022319a5c29a320acdfd4fd598b1c68f7223
204G1A0539 <204G1A0539@srit.ac.in>
2022-11-03 15:54:38 +0530

    first execution output

--- OSLAB/OPTIMAL.c
+++ OSLAB/OPTIMAL.c
@@ -5,7 +5,7 @@
    Date of Execution: 1-11-2022
 */
 #include<stdio.h>
-#include<conio.h>
+
 int fr[3], n, m;
 void
 display();
@@ -15,7 +15,7 @@ int i,j,page[20],fs[10];
 int
 max,found=0,lg[3],index,k,l,flag1=0,flag2=0,pf=0;
 float pr;
-clrscr();
+
 printf("Enter length of the reference string: ");
 scanf("%d",&n);
 
@@ -93,7 +93,7 @@ display();
 }
 printf("Number of page faults : %d\n", pf);
 pr=(float)pf/n*100;
-printf("Page fault rate = %f \n", pr); getch();
+printf("Page fault rate = %f \n", pr); 
 }
 void display()
 {
