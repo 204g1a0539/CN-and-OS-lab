405e56ae7992648fd5daebadfc8aae37da165804
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-11-03 16:20:31 +0530

    Create bestfit.c

---
+++ OSLAB/bestfit.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: memory allocation techniques Using bestfit Algorithm
+   Date of Creation: 1-11-2022
+   Date of Execution: 1-11-2022
+*/
