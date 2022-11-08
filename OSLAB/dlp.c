20ac513b6c36686793150fcf7507c9874d0a4d0d
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-11-08 12:40:27 +0530

    Create dlp.c

---
+++ OSLAB/dlp.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: dead lock avoidance using bankers Algorithm
+   Date of Creation: 8-11-2022
+   Date of Execution: 8-11-2022
+*/
