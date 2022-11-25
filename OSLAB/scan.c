efd0225417cc5c7ef57a693d4a85f556ea8501a1
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-11-25 20:44:26 +0530

    Create scan.c

---
+++ OSLAB/scan.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: DISK scheduling algorithms
+   Date of Creation: 25-11-2022
+   Date of Subbmission:25-11-2022
+   */
