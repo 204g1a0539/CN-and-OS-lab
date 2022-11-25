ec8065e55107bfbd17521ca05f072b209b258b94
204g1a0539 <94514853+204g1a0539@users.noreply.github.com>
2022-11-25 20:29:05 +0530

    Create sequential.c

---
+++ OSLAB/sequential.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: K.Hemalatha
+   Roll No: 204G1A0539
+   Title of the Experiment: file allocation strategies
+   Date of Creation: 25-11-2022
+   Date of Execution: 25-11-2022
+*/
